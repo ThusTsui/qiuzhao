93fa293282a0df9778284a83c1ad22e29d2c61af
czs <z522424994@163.com>
2023-10-04 23:12:48 +0800

    共享内存、消息队列

--- Linux/1.c
+++ Linux/1.c
@@ -1,25 +1,14 @@
-void Daemon()
-{
-    const int MAXFD = 64;
-    int i = 0;
-    if (fork() != 0)
-    {
-        exit(0);
-    }           // 父进程退出
-    setsid();   // 成为新进程组组长和新会话领导，脱离控制终端
-    chdir("/"); // 设置工作目录为根目录
-    umask(0);   // 重设文件访问权限掩码
-    for (; i < MAXFD; i++)
-    {
-        close(i); // 尽可能关闭所有从父进程继承来的文件
-    }
-}
+#include <func.h>
 int main()
 {
-    Daemon(); // 成为守护进程
-    while (1)
-    {
-        sleep(1);
-    }
-    return 0;
-}
+int semArrid = semget(1000,1,IPC_CREAT|0600);
+ERROR_CHECK(semArrid,-1,"semget");
+int ret = semctl(semArrid,0,SETVAL,1);
+ERROR_CHECK(ret,-1,"semctl");
+struct sembuf P;
+P.sem_num = 0;//信号量值在集合中的下标
+P.sem_op = -1;//信号量值修改
+P.sem_flg = SEM_UNDO;//如果P操作被终止，会自动恢复
+struct sembuf V;
+V.sem_num = 0;
+V.sem_op = 1;
